e261ca9728815723fe5baaacb4a1717d5f9eb83b
Artem LD31D <artem.12m21@gmail.com>
2020-12-29 18:15:19 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup 
+
+
+setup(
+	name='planfix_py',
+	version='0.1',
+	license='MIT',
+	description='',
+
+	packages=['planfix_py'],
+
+	url='https://github.com/LD31D/planfix_py',
+
+	author='LD31D',
+	author_email='artem.12m21@gmail.com',
+
+	zip_safe=False
+)
