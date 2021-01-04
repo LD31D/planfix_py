0b07f695b6b7a5f04055e81ab92fdab816b3a641
Artem LD31D <artem.12m21@gmail.com>
2021-01-04 23:28:06 +0200

    Updated Contact class for future

--- planfix_py/functions/contact.py
+++ planfix_py/functions/contact.py
@@ -2,4 +2,70 @@ class Contact(object):
 	
 	def __init__(self, base):
 		self = base
-	
+
+	def add(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.add
+		"""
+		pass
+
+	def update(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.update
+		"""
+		pass
+
+	def update_custom_data(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.updateCustomData
+		"""
+		pass
+
+	def get(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.get
+		"""
+		pass
+
+	def get_list(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.getList
+		"""
+		pass
+
+	def manage_planfix_access(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.managePlanfixAccess
+		"""
+		pass
+
+	def update_user_info(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.updateUserInfo
+		"""
+		pass
+
+	def update_contractors(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.updateContractors
+		"""
+		pass
+
+	def get_phone_types(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.getPhoneTypes
+		"""
+		pass
+
+	def get_group_list(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.getGroupList
+		"""
+		pass
+
+	def delete(self):
+		"""
+		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.delete
+		"""
+		pass
+		
