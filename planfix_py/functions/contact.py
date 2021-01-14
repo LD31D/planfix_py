9068497e4c4ccd50e4c9fe9fac3e7d9c72957839
Artem LD31D <artem.12m21@gmail.com>
2021-01-14 18:56:59 +0200

    Added contact.managePlanfixAccess

--- planfix_py/functions/contact.py
+++ planfix_py/functions/contact.py
@@ -55,11 +55,15 @@ class Contact(object):
 
 		return response
 
-	def manage_planfix_access(self):
+	def manage_planfix_access(self, kwargs):
 		"""
 		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.managePlanfixAccess
 		"""
-		pass
+		TEMPLATE_NAME = self.TEMPLATE_FOLDER + 'managePlanfixAccess.xml'
+
+		response = self.__base__._get_response(TEMPLATE_NAME, kwargs)
+
+		return response
 
 	def update_user_info(self):
 		"""
