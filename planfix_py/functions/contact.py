c5dc19a9106de65b34fad83aa6713e2a5e2fac4a
Artem LD31D <artem.12m21@gmail.com>
2021-01-20 21:49:36 +0200

    Added testing for contact

--- planfix_py/functions/contact.py
+++ planfix_py/functions/contact.py
@@ -105,7 +105,6 @@ class Contact(object):
 
 		return response
 
-
 	def delete(self, kwargs):
 		"""
 		https://planfix.ru/docs/%D0%9F%D0%BB%D0%B0%D0%BD%D0%A4%D0%B8%D0%BA%D1%81_API_contact.delete
---
+++ tests/test_contact.py
@@ -0,0 +1,71 @@
+import unittest
+
+from planfix_py import PlanfixAPI
+
+
+class TestContact(unittest.TestCase):
+
+	COMING_RESPONSE = '<?xml version="1.0" encoding="UTF-8"?>\n<response status="error"><code>0001</code><message/></response>\n'
+
+	def setUp(self):
+		self.pf = PlanfixAPI()
+
+	def test_contact_add(self):
+		response = self.pf.contact.add({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_update(self):
+		response = self.pf.contact.update({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_update_custom_data(self):
+		response = self.pf.contact.update_custom_data({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_get(self):
+		response = self.pf.contact.get({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_get_list(self):
+		response = self.pf.contact.get_list({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_manage_planfix_access(self):
+		response = self.pf.contact.manage_planfix_access({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_update_user_info(self):
+		response = self.pf.contact.update_user_info({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_update_contractors(self):
+		response = self.pf.contact.update_contractors({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_get_phone_types(self):
+		response = self.pf.contact.get_phone_types({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_get_group_list(self):
+		response = self.pf.contact.get_group_list({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+	def test_contact_delete(self):
+		response = self.pf.contact.delete({})
+
+		self.assertEqual(self.COMING_RESPONSE, response)
+
+
+if __name__ == '__main__':
+	unittest.main()
+	
